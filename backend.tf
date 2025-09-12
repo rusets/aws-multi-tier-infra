2513f1f1958e26471798a85d0630a158c19bed07
rusets <rusets3@gmail.com>
2025-09-12 13:24:12 -0500

    CI/CD: remote backend + GitHub OIDC role + infra workflow

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 # Terraform remote state: S3 backend with DynamoDB locking
 terraform {
   backend "s3" {
-    bucket         = "multi-tier-demo-tfstate-097635932419-566ddc" # S3 bucket for state
+    bucket         = "multi-tier-demo-tfstate-097635932419-e7f2c4" # S3 bucket for state
     key            = "aws-multi-tier-infra/terraform.tfstate"      # state object key
     region         = "us-east-1"                                   # backend region
     dynamodb_table = "multi-tier-demo-tf-locks"                    # state lock table (DynamoDB)
